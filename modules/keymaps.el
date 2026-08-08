37b90d51a2f285230dc663cad73a694f03ee8041
desonglll <lindesong666@163.com>
2026-08-08 17:24:05 +0800

    update

---
+++ modules/keymaps.el
@@ -0,0 +1,26 @@
+
+;;; keymaps.el --- keymap configuration -*- lexical-binding: t; -*-
+
+;;;; Global key bindings
+
+(dolist (binding
+         '(("M-<f2>" . dirvish)
+           ("C-," . duplicate-line)
+           ("C-:" . avy-goto-char-2)
+           ("s-\\" . avy-goto-char-2)
+           ("s-u" . revert-buffer)
+           ("s-i" . imenu-list)
+           ("s-e" . treemacs)
+           ("M-o" . ace-window)
+           ("C-c RET" . ffap)
+           ("C-c f r" . recentf)
+           ("C-c f f" . consult-fd)
+           ("C-c f h" . consult-ripgrep)
+           ("s-<return>" . my/new-buffer)
+           ("C-<tab>" . next-buffer)
+           ("C-S-<tab>" . previous-buffer)
+           ("C-c b" . my/list-buffers-focus)
+           ))
+  (keymap-global-set (car binding) (cdr binding)))
+
+;;; keymaps.el ends here.
